ec3231e81b14d6931fe3b5374c7f4eb8f36b76a7
Destin Lee <dleeinc@gmail.com>
2021-01-12 08:34:21 -0600

    testing router

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,19 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import GlobalStyle from '../styles/styles'
 
+import About from './About'
+import Home from './Home'
+
 // If you would like to use bootstrap uncomment below
 // import '../styles/bootstrap.min.css'
-
-function About() {
-  return <h1>About</h1>
-}
-
-function Home() {
-  return <h1>Home</h1>
-}
-
-function Nav() {
-  return (
-    <div>
-      <Link to="/about">about1</Link>
-      <Link to="/">home</Link>
-    </div>
-  )
-}
 
 export default function App() {
   return (
     <>
       <GlobalStyle />
       <h1>Welcome</h1>
-      <Nav />
       <Switch>
         <Route path="/about">
           <About />
