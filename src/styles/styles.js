1732ac6cad2802a3d6ffeadad0328f94bf88e956
Destin Lee <dleeinc@gmail.com>
2021-01-13 16:55:58 -0600

    css resets

--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3,12 +3,43 @@ import { createGlobalStyle } from 'styled-components'
 const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
+  }
+
+  html,
+  body {
     margin: 0;
     padding: 0;
   }
 
   body {
+    color: #111;
     font-family: sans-serif;
+  }
+
+  a {
+    text-decoration: none;
+
+    &:visited {
+      color: #111;
+    }
+  }
+
+  ul,
+  ol {
+    margin: 0 0 0 1rem;
+    padding: 0 0 0 1rem;
+  }
+
+  table {
+    border-collapse: collapse;
+  }
+
+  th {
+    text-align: left;
+  }
+
+  td {
+    vertical-align: middle;
   }
 `
 export default GlobalStyle
